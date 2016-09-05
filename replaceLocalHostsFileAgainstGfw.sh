45c92b03989dddf85b66737bc5eaea36e2723577
Guodong Ding <DingGuodong@users.noreply.github.com>
2016-09-05 16:22:55 +0800

    Update replaceLocalHostsFileAgainstGfw.sh

--- replaceLocalHostsFileAgainstGfw.sh
+++ replaceLocalHostsFileAgainstGfw.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Function description:
-#GoogleHostsFileForLinux
-#Google Hosts File For Linux, a Linux shell script make you access Google easily! Run it directly and enjoy Google services.
+# Google Hosts File For Linux, a Linux shell script make you access Google easily! Run it directly and enjoy Google services.
 #
 # Usage:
 # bash replaceLocalHostsFileAgainstGfw.sh
